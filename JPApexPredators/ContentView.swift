6feff1f2ab8ba8cbffc43294206c8fbcdf25687a
Collin-Schmitt <collinanglese23@gmail.com>
2025-04-10 15:11:25 -0500

    Initial Commit

---
+++ JPApexPredators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredators
+//
+//  Created by Collin Schmitt on 4/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
